9a876a8d91f5b372e640534b6236093b0232b4b2
Ibad Uddin <ibaduddinsiddiqui418@gmail.com>
2025-02-12 13:59:35 +0500

    hello_world Strings variables

---
+++ basics/strings_and _methods.py
@@ -0,0 +1,21 @@
+# strings and their methods
+name = "ibad uddin"
+
+print(name.title()) #title() method
+print(name.upper()) #upper() method
+print(name.lower()) #lower() method
+
+first_name = "ammad"
+last_name = "uddin"
+full_name = f"{first_name} {last_name}"
+# The f before the string makes it an f-string (formatted string)
+# It allows us to embed expressions inside string literals using {}
+# Here it combines first_name and last_name into full_name
+print(full_name)  # This will print: ammad uddin
+
+
+
+
+
+
+
---
+++ basics/varaibles.py
@@ -0,0 +1,23 @@
+
+# variables in python
+
+message = "Assalmualikum"
+
+print(message);
+
+message = "hi ibad"  #redeclaring a variable
+
+print(message,"afterwards") 
+
+
+message2 = "ibad is here"
+message  = message2 #assignning value of one variable to another
+
+print (message2,"message 2")
+
+
+
+
+
+
+
